f363672b60d61c67efb210afb53d65e16c52297e
horasachy <misha89891@mail.ru>
2020-08-29 16:45:27 +0500

    customer - auth

--- jangoMiniShop/account/urls.py
+++ jangoMiniShop/account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from django.contrib.auth import views as AuthView
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name='register'),
+    path('login', views.login_view, name='login'),
+    path('register', views.register_view, name='register'),
+    path('logout/', AuthView.LogoutView.as_view(), name='logout'),
 ]
 
--- jangoMiniShop/account/views.py
+++ jangoMiniShop/account/views.py
@@ -1,7 +1,41 @@
-from django.http import HttpResponse
-from pprint import pprint
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import CreateUserForm, LoginUserForm
+from django.shortcuts import render, redirect
 
 
-def register(request):
-    return render(request, 'account/register.html')
+def register_view(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('products')
+
+    context = {'form': form}
+    return render(request, 'account/register.html', context)
+
+
+def login_view(request):
+    form = AuthenticationForm()
+
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('products')
+
+    context = {'form': form}
+    return render(request, 'account/login.html', context)
+
+
+def logout_view(request):
+    logout(request)
